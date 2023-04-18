46c6dbe5a317eb06aa5430b0da59f601444b3ac5
Kevin Balmores <balmoresk3v1n@gmail.com>
2023-04-18 18:41:11 +0800

    changed app brand name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,7 +60,7 @@ export const Navbar = () => {
             <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
               <Link to='/' className="text-decoration-none d-block d-lg-none">
                 <span className="h1 text-uppercase text-dark bg-light px-2">
-                  React
+                  Go
                 </span>
                 <span className="h1 text-uppercase text-light bg-primary px-2 ml-n1">
                   Shop
